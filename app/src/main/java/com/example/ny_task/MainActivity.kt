172f5d2847da074f185a7a9c5f4ec7db8cd738a5
ELTEGANI MOHAMED <elteganimohamed@ELTEGANIs-MacBook-Air.local>
2022-01-19 23:01:22 +0400

    add ui navigation

--- app/src/main/java/com/example/ny_task/MainActivity.kt
+++ app/src/main/java/com/example/ny_task/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.ny_task
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.ny_task.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var activityMainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
